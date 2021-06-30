0715573b8d39858878090c64afe698d059cd7a25
srjsingh <Srjsingh636@gmail.com>
2021-07-01 00:15:47 +0530

    ListConcept

---
+++ CollectionsConcept/src/ListConcept/ArrayListConcept.java
@@ -0,0 +1,12 @@
+package ListConcept;
+
+public class ArrayListConcept {
+
+	public static void main(String[] args) {
+
+		int a[] = new int[3]; // static array
+		
+		// dynamic array -> ArrayList
+	}
+
+}
