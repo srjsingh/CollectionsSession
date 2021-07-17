a3cd60492b4ad09084a63e46d230e4c8840a1a36
srjsingh <Srjsingh636@gmail.com>
2021-07-17 13:21:25 +0530

    changes

--- CollectionsConcept/src/finalVSfinallyVSfinalize/FinalConcept.java
+++ CollectionsConcept/src/finalVSfinallyVSfinalize/FinalConcept.java
@@ -4,7 +4,7 @@ public class FinalConcept {
 
 	public static void main(String[] args) {
 
-		final int i = 20; //final keyword is used for defining constants
+		final int i = 20; //final keyword is used for defining constants, preventing inheritance and method overriding
 		//i = 30;
 		//i = 40;
 		System.out.println(i);
